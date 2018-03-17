260cc08678b80b6acc16248414f3bb1dc6bee15a
RazielLewach <37403178+RazielLewach@users.noreply.github.com>
2018-03-17 22:51:06 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,6 @@
-var express = require('express'),
-    fs = require('fs'),
-    app = express();
-//    eps = require('ejs'),
-//    morgan = require('morgan');
+var http = require('http');
 
-var app = express();
-
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-
-
-// app is running!
-app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date());
-});
-
-
-
-app.listen(8080, ip);
-
-
-
-module.exports = app;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
